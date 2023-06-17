d310bdeb25970f709ad9bad01be3f7cf21e89e02
DinossauroBebado <guipsilva32@gmail.com>
2023-06-16 22:07:58 -0300

    failed_try to program motors

--- Main/Firmware/src/examples/motor/main.cpp
+++ Main/Firmware/src/examples/motor/main.cpp
@@ -1,28 +1,45 @@
-#include <Arduino.h>
+// * Note: the following code is a modification of the Knob Example that
+// * comes with the ESC library, so that it works with an ESP32 DevKit V1
+// * and my particular ESC (generic 30A) and brushless motor (generic 2200KV)
+// */
 
+#include <ESP32Servo.h> // ESP32Servo library installed by Library Manager
+#include <ESC.h> // RC_ESP library installed by Library Manager
 
-int POT = 25;
-int SIGNAL = 33;
-int pot_reading = 0; 
+#define ESC_PIN (33) // connected to ESC control wire
+#define LED_BUILTIN (2) // not defaulted properly for ESP32s/you must define it
+#define POT_PIN (25) // Analog pin used to connect the potentiometer center pin
 
-void debug(int pot){
+// Note: the following speeds may need to be modified for your particular hardware.
+#define MIN_SPEED 1040 // speed just slow enough to turn motor off
+#define MAX_SPEED 1240 // speed where my motor drew 3.6 amps at 12v.
 
-Serial.print("pot: ");
-Serial.print(pot);
-Serial.println("");
+ESC myESC (ESC_PIN, 1500, 2500, 800); // ESC_Name (PIN, Minimum Value, Maximum Value, Arm Value)
 
+long int val; // variable to read the value from the analog pin
+
+void setup() {
+Serial.begin(112500);
+delay(1000);
+pinMode(POT_PIN, INPUT);
+pinMode(ESC_PIN, OUTPUT);
+pinMode(LED_BUILTIN, OUTPUT);
+digitalWrite(LED_BUILTIN, HIGH); // set led to on to indicate arming
+myESC.arm(); // Send the Arm command to ESC
+delay(5000); // Wait a while
+digitalWrite(LED_BUILTIN, LOW); // led off to indicate arming completed
+
+// the following loop turns on the motor slowly, so get ready
+for (int i=0; i<350; i++){ // run speed from 840 to 1190
+myESC.speed(MIN_SPEED-200+i); // motor starts up about half way through loop
+delay(10);
 }
+} // speed will now jump to pot setting
 
-void setup() 
-{
-    Serial.begin(112500); 
-
-    //pot  0 - 4095
-    pinMode(POT,INPUT);
-}
-
-void loop() 
-{   
-    pot_reading = analogRead(POT);
-    debug(pot_reading);
+void loop() {
+val = analogRead(POT_PIN); // read the pot (value between 0 and 4095 for ESP32 12 bit A to D)
+Serial.println(val);
+val = map(val, 0, 4095, MIN_SPEED, MAX_SPEED); // scale pot reading to valid speed range
+myESC.speed(val); // sets the ESC speed
+delay(10); // Wait for a while
 }
